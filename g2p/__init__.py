cb42b154e59c2cc1be5cf8394c0d93336b763162
roedoejet <aidanpine@shaw.ca>
2019-05-31 23:34:51 -0700

    added basic g2p studio flask app

--- g2p/__init__.py
+++ g2p/__init__.py
@@ -1,1 +1,9 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 VERSION = '0.0.1'
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():    
+    return render_template('index.html')
---
+++ run_studio.py
@@ -0,0 +1,3 @@
+from g2p import app
+
+app.run(host='0.0.0.0', port=5000, debug=True)
