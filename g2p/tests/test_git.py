2a996a6ff56e70041310143cab020e25c34d3e5e
roedoejet <aidanpine@shaw.ca>
2019-07-09 10:49:50 -0700

    git test skeleton

--- g2p/tests/test_git.py
+++ g2p/tests/test_git.py
@@ -1,50 +1,82 @@
 # -*- coding: utf-8 -*-
 
-from unittest import main, TestCase
 import os
+from unittest import main, TestCase
 from g2p.cors import Correspondence
 from g2p.transducer import Transducer
-from datawrangler import data_wrangler
+# Will cause errors on machines without private data
+from g2p.tests.private.git_data_wrangler import returnLinesFromDocuments
+from g2p.tests.private import __file__ as private_dir
+from typing import List
 
-#remove ['~', '-', '='] from apa data
 
-apa_text.replace(['~', '-', '='] , "")
-apa_text = apa_text.replace
+def scrub_text(txt: str, to_scrub: List[str] = ['=', '-', '~']) -> str:
+    ''' Given some text (txt), scrub all characters in list (to_scrub) from text.
+    '''
+    for char in to_scrub:
+        txt = txt.replace(char, '')
+    return txt
+
 
 class GitTest(TestCase):
     ''' Test for Orthography to APA
     '''
-  def setUp(self):
+
+    def setUp(self):
         git = {"name": "git",
                "tables": {
-                   "Orthography (Step 1)": [                     
-                       ("Hli dii ap wehl Gitwinhlguu'l, Git-anyaaw, wil mig\u0332oontxw Git-anyaaw.", "Hli diː æb wiɬ Gidwinɬɟulˀ, Gid-ænjæː|ʌw, wil miɟ\u0332ɔ|ontxʷ Gid-ænjæː|ʌw"),
-                       ("Ii 'nithl sim'oogit Gwaas Hlaa'm 'nii ksg\u0332oog\u0332at ahl lax\u0332 g\u0332alts'ap tun.", "Ii ʔnidɬ simʔɔ|oːɟid Gwæː|ʌːs Hlæː|ʌːʔm ʔniː ksɟ\u0332ɔ|oːɟ\u0332æd æɬ læx\u0332 ɟ\u0332ælʦˀæb tun.") 
+                   "Orthography (Step 1)": [
+                       ("Hli dii ap wehl Gitwinhlguu'l, Git-anyaaw, wil mig\u0332oontxw Git-anyaaw.",
+                        "Hli diː æb wiɬ Gidwinɬɟulˀ, Gid-ænjæː|ʌw, wil miɟ\u0332ɔ|ontxʷ Gid-ænjæː|ʌw"),
+                       ("Ii 'nithl sim'oogit Gwaas Hlaa'm 'nii ksg\u0332oog\u0332at ahl lax\u0332 g\u0332alts'ap tun.",
+                        "Ii ʔnidɬ simʔɔ|oːɟid Gwæː|ʌːs Hlæː|ʌːʔm ʔniː ksɟ\u0332ɔ|oːɟ\u0332æd æɬ læx\u0332 ɟ\u0332ælʦˀæb tun.")
                    ],
                    "APA (no free variation)": [
-                        ("", "\u026c\u0259 ti: \u0294\u0259p we=\u026c k\u0259tw\u0259n\u026ck\u02b7u:l\u0313 k\u0259t\u0294\u0259nya:w w\u0259l m\u0259qo:ntx\u02b7 k\u0259t-\u0294\u0259n-ya:w"),
-                        ("", "\u0294i: n\u0313it=\u026c s\u0259m\u0294o:k\u0259t k\u02b7a:s-\u026ca:m\u0313 n\u0313i: ksqo:q-\u0259t \u0294\u0259=\u026c l\u0259\u03c7 q\u0259lc\u0313ap t=x\u02b7\u0259n")
+                       ("", "\u026c\u0259 ti: \u0294\u0259p we=\u026c k\u0259tw\u0259n\u026ck\u02b7u:l\u0313 k\u0259t\u0294\u0259nya:w w\u0259l m\u0259qo:ntx\u02b7 k\u0259t-\u0294\u0259n-ya:w"),
+                       ("", "\u0294i: n\u0313it=\u026c s\u0259m\u0294o:k\u0259t k\u02b7a:s-\u026ca:m\u0313 n\u0313i: ksqo:q-\u0259t \u0294\u0259=\u026c l\u0259\u03c7 q\u0259lc\u0313ap t=x\u02b7\u0259n")
                    ],
-                   }
                }
+               }
+        self.formatted_data = returnLinesFromDocuments(
+            [
+                os.path.join(os.path.dirname(private_dir),
+                             'VG - Nass Volcano',
+                             '2016-07-12 Nass River Volcano - CF edit VG check2.docx'),
+                os.path.join(os.path.dirname(private_dir),
+                             'VG - Kitwancool Surveyed',
+                             '2014-01-29 VG Kitwancool Reserve Surveyed CAF edit3 for HD.docx'),
+                os.path.join(os.path.dirname(private_dir),
+                             'VG - Founding of Gitanyow',
+                             '2017-01-27 VG The Founding of Git-anyaaw - HD comments more fixes.docx'),
+                os.path.join(os.path.dirname(private_dir),
+                             "HH - Mystery Story",
+                             "15-10-31 HH Hector's Betl'a Betl' - 4 checks_AP.docx"),
+                os.path.join(os.path.dirname(private_dir),
+                             "HH - Win Bekwhl",
+                             "2014-10-22 HH Win Bexwhl K'amk'siwaa - 2016-04-22 CAF 1st gloss.docx"),
+                os.path.join(os.path.dirname(private_dir),
+                             "BS - Dihlxw",
+                             "Dihlxw Story 2013-04-29 for HD copy - clean.docx"),
+            ])
 
-        self.langs_to_test = [git]
+        # Declare all of our correspondences needed
+        self.orth_to_ipa = Correspondence(
+            language={"lang": "git", "table": "Orthography (Step 1)"})
+        self.orth_to_ipa_transducer = Transducer(self.orth_to_ipa)
 
-    def test_io(self):
-        # go through each language declared in the test case set up
-        for lang in self.langs_to_test:
-            lang_name = lang['name']
-            # go through each table in the current lang
-            for table in lang['tables'].keys():
-                cors = Correspondence(
-                    language={'lang': lang_name, 'table': table})
-                transducer = Transducer(cors, as_is=True)
-                # go through each input/output pair
-                for pair in lang['tables'][table]:
-                    # assert that the transduced first item in the tuple is equal to the second item in the tuple
-                    self.assertEqual(transducer(pair[0]), pair[1])
+        self.apa_to_ipa = Correspondence(
+            language={"lang": "git", "table": "APA (free variation)"})
+        self.apa_to_ipa_transducer = Transducer(self.apa_to_ipa)
 
+        self.ipa_to_apa = Correspondence(
+            language={"lang": "git", "table": "APA (free variation)"}, reverse=True)
+        self.ipa_to_apa_transducer = Transducer(self.ipa_to_apa)
 
+    def test_orth_to_apa(self):
+        for story in self.formatted_data:
+            for line in story:
+                self.assertEqual(self.ipa_to_apa_transducer(
+                    self.orth_to_ipa_transducer(line['ortho'])), scrub_text(line['apa']))
 
 if __name__ == "__main__":
     main()
