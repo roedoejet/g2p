f7b3ad8c1554a69733d9f452f8d116dc83aa3214
Eddie Antonio Santos <easantos@ualberta.ca>
2019-07-18 15:05:10 -0600

    Extract VERSION into its own file, for unfortunate reasons.

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 ''' Setup for G2P
 '''
 from setuptools import setup, find_packages
-import g2p
+
+# Ugly hack to read the current version number without importing g2p:
+# (works by )
+with open("g2p/__version__.py", "r", encoding="UTF-8") as version_file:
+    namespace = {}
+    exec(version_file.read(), namespace)
+    VERSION = namespace['VERSION']
 
 setup(
     name='g2p',
     python_requires='>=3.6',
-    version=g2p.VERSION,
+    version=VERSION,
     long_description='indexed grapheme to phoneme conversion',
     packages=find_packages(),
     include_package_data=True,
