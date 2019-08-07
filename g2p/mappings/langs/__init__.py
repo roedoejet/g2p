5fd813331c5b04a4d5470e43903c66168c185f5a
roedoejet <aidanpine@shaw.ca>
2019-08-06 17:38:07 -0700

    added more helpful available languages

--- g2p/mappings/langs/__init__.py
+++ g2p/mappings/langs/__init__.py
@@ -1,5 +1,6 @@
 import os
 import yaml
+from copy import deepcopy
 
 # LANGS = {}
 
@@ -8,3 +9,16 @@ with open(os.path.join(os.path.dirname(__file__), 'langs.yml'), encoding='utf-8'
         LANGS = yaml.safe_load(f)
     except yaml.YAMLError as exc:
         print(exc)
+
+LANGS_AVAILABLE = [{x['code']: x['name']} for x in LANGS]
+
+TABLES_AVAILABLE = []
+for lang in LANGS:
+    new_lang = {}
+    for k, v in deepcopy(lang).items():
+        if k == 'code':
+            current_lang = v
+            new_lang[v] = []
+        if k == 'tables':
+            new_lang[current_lang] += [x['name'] for x in v]
+    TABLES_AVAILABLE.append(new_lang)
