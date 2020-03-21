aa849ffa679131085f60e840a1678bb53c95bfce
David Huggins-Daines <dhdaines@gmail.com>
2020-03-21 19:58:38 -0400

    add a bit of documentation

--- g2p/tests/test_langs.py
+++ g2p/tests/test_langs.py
@@ -12,7 +12,14 @@ from g2p.tests.public.data import __file__ as data_dir
 
 
 class LangTest(TestCase):
-    ''' Basic Test for individual lookup tables
+    '''Basic Test for individual lookup tables.
+
+    Test files (in g2p/tests/public/data) are either .csv, .psv, or
+    .tsv files, the only difference being the delimiter used (comma,
+    pipe, or tab).
+
+    Each line in the test file consists of SOURCE,TARGET,INPUT,OUTPUT
+
     '''
 
     def setUp(self):
