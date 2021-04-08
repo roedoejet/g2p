f4ace7e215baff4a0caa223227982da09c8466eb
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2021-04-08 13:20:57 -0400

    perf: script to measure speed of different panphon.distance.Distance() init solutions

---
+++ g2p/tests/time_panphon.py
@@ -0,0 +1,24 @@
+""" Time how long it takes to initialize panphon.distance.Distance() repeatedly,
+    and how fast is_panphon() is when using various singleton or non-singleton solutions.
+    (To benchmark the different solutions, I changed mappings.langs.utils.is_panphon and
+    reran this timing script.)
+"""
+
+from linetimer import CodeTimer
+from panphon import distance
+from g2p.mappings.langs.utils import is_panphon
+
+for words in (1, 10):
+    with CodeTimer(f"is_panphon() {words} words"):
+        string = " ".join(["ei" for i in range(words)])
+        is_panphon(string)
+
+    with CodeTimer(f"is_panphon() on 1 word {words} times"):
+        string = "ei"
+        for i in range(words):
+            is_panphon(string)
+
+for iters in (1, 10):
+    with CodeTimer(f"dst init {iters} times"):
+        for i in range(iters):
+            dst = distance.Distance()
