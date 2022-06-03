460deb0a01a3c39e649c6aac46950fb9cfec4fd2
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2022-06-03 12:16:15 -0400

    style: blackify test_create_mapping.py

--- g2p/tests/test_create_mapping.py
+++ g2p/tests/test_create_mapping.py
@@ -18,20 +18,21 @@ from g2p.transducer import Transducer
 class MappingCreationTest(TestCase):
     def setUp(self):
         self.mappings = [
-            {"in": "ɑ",	"out": "AA"},
-            {"in": "eː",	"out": "EY"},
-            {"in": "i",	"out": "IY"},
-            {"in": "u",	"out": "UW"},
-            {"in": "tʃ",	"out": "CH"},
-            {"in": "p",	"out": "P"},
-            {"in": "t",	"out": "T"},
-            {"in": "k",	"out": "K"},
-            {"in": "w",	"out": "W"},
+            {"in": "ɑ", "out": "AA"},
+            {"in": "eː", "out": "EY"},
+            {"in": "i", "out": "IY"},
+            {"in": "u", "out": "UW"},
+            {"in": "tʃ", "out": "CH"},
+            {"in": "p", "out": "P"},
+            {"in": "t", "out": "T"},
+            {"in": "k", "out": "K"},
+            {"in": "w", "out": "W"},
             {"in": "ɡ", "out": "G"},
             {"in": "ʒ", "out": "ZH"},
         ]
         self.target_mapping = Mapping(
-            self.mappings, in_lang='eng-ipa', out_lang='eng-arpabet', out_delimiter=' ')
+            self.mappings, in_lang="eng-ipa", out_lang="eng-arpabet", out_delimiter=" "
+        )
 
     def test_unigram_mappings(self):
         src_mappings = [
@@ -39,12 +40,12 @@ class MappingCreationTest(TestCase):
             {"in": "ᐅ", "out": "u"},
             {"in": "ᐊ", "out": "a"},
         ]
-        src_mapping = Mapping(src_mappings, in_lang='crj', out_lang='crj-ipa')
+        src_mapping = Mapping(src_mappings, in_lang="crj", out_lang="crj-ipa")
         mapping = create_mapping(src_mapping, self.target_mapping)
         transducer = Transducer(mapping)
-        self.assertEqual(transducer('a').output_string, 'ɑ')
-        self.assertEqual(transducer('i').output_string, 'i')
-        self.assertEqual(transducer('u').output_string, 'u')
+        self.assertEqual(transducer("a").output_string, "ɑ")
+        self.assertEqual(transducer("i").output_string, "i")
+        self.assertEqual(transducer("u").output_string, "u")
 
     def test_bigram_mappings(self):
         src_mappings = [
@@ -52,12 +53,12 @@ class MappingCreationTest(TestCase):
             {"in": "ᑎ", "out": "ti"},
             {"in": "ᑭ", "out": "ki"},
         ]
-        src_mapping = Mapping(src_mappings, in_lang='crj', out_lang='crj-ipa')
+        src_mapping = Mapping(src_mappings, in_lang="crj", out_lang="crj-ipa")
         mapping = create_mapping(src_mapping, self.target_mapping)
         transducer = Transducer(mapping)
-        self.assertEqual(transducer('pi').output_string, 'pi')
-        self.assertEqual(transducer('ti').output_string, 'ti')
-        self.assertEqual(transducer('ki').output_string, 'ki')
+        self.assertEqual(transducer("pi").output_string, "pi")
+        self.assertEqual(transducer("ti").output_string, "ti")
+        self.assertEqual(transducer("ki").output_string, "ki")
 
     def test_trigram_mappings(self):
         src_mappings = [
@@ -65,12 +66,12 @@ class MappingCreationTest(TestCase):
             {"in": "ᒍ", "out": "t͡ʃu"},
             {"in": "ᒐ", "out": "t͡ʃa"},
         ]
-        src_mapping = Mapping(src_mappings, in_lang='crj', out_lang='crj-ipa')
+        src_mapping = Mapping(src_mappings, in_lang="crj", out_lang="crj-ipa")
         mapping = create_mapping(src_mapping, self.target_mapping)
         transducer = Transducer(mapping)
-        self.assertEqual(transducer('t͡ʃi').output_string, 'tʃi')
-        self.assertEqual(transducer('t͡ʃu').output_string, 'tʃu')
-        self.assertEqual(transducer('t͡ʃa').output_string, 'tʃɑ')
+        self.assertEqual(transducer("t͡ʃi").output_string, "tʃi")
+        self.assertEqual(transducer("t͡ʃu").output_string, "tʃu")
+        self.assertEqual(transducer("t͡ʃa").output_string, "tʃɑ")
 
     def test_long_mappings(self):
         src_mappings = [
@@ -78,12 +79,12 @@ class MappingCreationTest(TestCase):
             {"in": "ᐧᑌ", "out": "tʷeː"},
             {"in": "ᐧᑫ", "out": "kʷeː"},
         ]
-        src_mapping = Mapping(src_mappings, in_lang='crj', out_lang='crj-ipa')
+        src_mapping = Mapping(src_mappings, in_lang="crj", out_lang="crj-ipa")
         mapping = create_mapping(src_mapping, self.target_mapping)
         transducer = Transducer(mapping)
-        self.assertEqual(transducer('pʷeː').output_string, 'pweː')
-        self.assertEqual(transducer('tʷeː').output_string, 'tweː')
-        self.assertEqual(transducer('kʷeː').output_string, 'kweː')
+        self.assertEqual(transducer("pʷeː").output_string, "pweː")
+        self.assertEqual(transducer("tʷeː").output_string, "tweː")
+        self.assertEqual(transducer("kʷeː").output_string, "kweː")
 
     def test_distance_errors(self):
         src_mappings = [{"in": "ᐃ", "out": "i"}]
@@ -143,5 +144,5 @@ class MappingCreationTest(TestCase):
         self.assertGreater(len(set_of_mappings), 3)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
