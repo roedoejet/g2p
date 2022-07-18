ea45afa8148ed9f805ad6ea808d67bc410fc5a19
David Huggins-Daines <dhdaines@gmail.com>
2022-07-18 13:58:27 -0400

    fix: ensure python g2p/mappings/langs/__init__.py can always run
    Move the loading of the pickle files to a function, which is
    not called if the module gets called directly.  Probably
    not the ideal solution since  should always run
    as well...

--- g2p/mappings/langs/__init__.py
+++ g2p/mappings/langs/__init__.py
@@ -1,3 +1,6 @@
+"""
+Language mappings for g2p.
+"""
 import os
 import pickle
 import timeit
@@ -5,20 +8,35 @@ from copy import deepcopy
 from pathlib import Path
 
 import yaml
+import argparse
 from networkx import DiGraph, read_gpickle, shortest_path, write_gpickle
 
 from g2p.exceptions import MappingMissing, MalformedMapping
 from g2p.mappings.utils import find_mapping, load_mapping_from_path
 
 LANGS_DIR = os.path.dirname(__file__)
-LANGS_PKL = os.path.join(LANGS_DIR, "langs.pkl")
-LANGS_NWORK_PATH = os.path.join(LANGS_DIR, "network.pkl")
+LANGS_PKL_NAME = "langs.pkl"
+LANGS_PKL = os.path.join(LANGS_DIR, LANGS_PKL_NAME)
+NETWORK_PKL_NAME = "network.pkl"
+LANGS_NWORK_PATH = os.path.join(LANGS_DIR, NETWORK_PKL_NAME)
 
 
-def cache_langs():
-    """Read in all files and save as pickle"""
+def cache_langs(dir_path: str = LANGS_DIR,
+                langs_path: str = LANGS_PKL,
+                network_path: str = LANGS_NWORK_PATH):
+    """Read in all files and save as pickle.
+
+    Args:
+       dir_path: Path to scan for config.yaml files.  Default is the
+                 installed g2p/mappings/langs directory.
+       langs_path: Path to output langs.pkl pickle file.  Default is
+                   the installed g2p/mappings/langs/langs.pkl
+       network_path: Path to output pickle file.  Default is the
+                     installed g2p/mappings/langs/network.pkl.
+    """
     langs = {}
-    dir_path = Path(LANGS_DIR)
+    dir_path = Path(dir_path)
+        
     # Sort by language code
     paths = sorted(dir_path.glob("./*/config.y*ml"), key=lambda x: x.parent.stem)
     mappings_legal_pairs = []
@@ -45,41 +63,63 @@ def cache_langs():
     lang_network = DiGraph()
     lang_network.add_edges_from(mappings_legal_pairs)
 
-    with open(LANGS_NWORK_PATH, "wb") as f:
+    with open(network_path, "wb") as f:
         write_gpickle(lang_network, f, protocol=4)
 
-    with open(LANGS_PKL, "wb") as f:
+    with open(langs_path, "wb") as f:
         pickle.dump(langs, f, protocol=4)
 
     return langs
 
-
-# Cache mappings as pickle file for quick loading
-with open(LANGS_PKL, "rb") as f:
-    LANGS = pickle.load(f)
-
-LANGS_NETWORK = read_gpickle(LANGS_NWORK_PATH)
-for k, v in LANGS.items():
-    if k in ["generated", "font-encodings"]:
-        continue
-    if "mappings" in v:
-        for vv in v["mappings"]:
-            if "language_name" not in vv:
-                raise MalformedMapping("language_name missing from mapping for " + k);
+LANGS = {}
+LANGS_NETWORK = DiGraph()
 LANGS_AVAILABLE = []
-for k, v in LANGS.items():
-    if k in ["generated", "font-encodings"]:
-        continue
-    if "mappings" in v:
-        LANGS_AVAILABLE.extend(vv["language_name"] for vv in v["mappings"])
-    else:
-        LANGS_AVAILABLE.append(v["language_name"])
 MAPPINGS_AVAILABLE = []
-for k, v in LANGS.items():
-    if "mappings" in v:
-        MAPPINGS_AVAILABLE.extend(v["mappings"])
-    else:
-        MAPPINGS_AVAILABLE.append(v)
+def load_langs(langs_path: str = LANGS_PKL):
+    global LANGS, LANGS_NETWORK, LANGS_AVAILABLE, MAPPINGS_AVAILABLE
+    # Cache mappings as pickle file for quick loading
+    with open(LANGS_PKL, "rb") as f:
+        LANGS = pickle.load(f)
+
+    LANGS_NETWORK = read_gpickle(LANGS_NWORK_PATH)
+    for k, v in LANGS.items():
+        if k in ["generated", "font-encodings"]:
+            continue
+        if "mappings" in v:
+            for vv in v["mappings"]:
+                if "language_name" not in vv:
+                    raise MalformedMapping(
+                        "language_name missing from mapping for " + k)
+    # language_name is not unique for each mapping
+    language_names = set()
+    for k, v in LANGS.items():
+        if k in ["generated", "font-encodings"]:
+            continue
+        if "mappings" in v:
+            for vv in v["mappings"]:
+                language_names.add(vv["language_name"])
+        else:
+            language_names.add(v["language_name"])
+    LANGS_AVAILABLE[:] = sorted(language_names)
+    del MAPPINGS_AVAILABLE[:]
+    for k, v in LANGS.items():
+        if "mappings" in v:
+            MAPPINGS_AVAILABLE.extend(v["mappings"])
+        else:
+            MAPPINGS_AVAILABLE.append(v)
+
 
 if __name__ == "__main__":
-    cache_langs()
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("-d", "--langdir",
+                        default=LANGS_DIR,
+                        help="Input directory to scan for language mappings.")
+    parser.add_argument("-o", "--outdir",
+                        default=LANGS_DIR,
+                        help="Ouptut diretory to write cache files.")
+    args = parser.parse_args()
+    cache_langs(dir_path=args.langdir,
+                langs_path=os.path.join(args.outdir, LANGS_PKL_NAME),
+                network_path=os.path.join(args.outdir, NETWORK_PKL_NAME))
+else:
+    load_langs()
