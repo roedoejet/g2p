cd3165733bbb5cd21b28aa2a3115cc13887dfa90
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2022-07-19 17:03:02 -0400

    style: blackify and isort a few more files

--- run_tests.py
+++ run_tests.py
@@ -8,6 +8,6 @@ from g2p.tests.run import run_tests
 try:
     result = run_tests(sys.argv[1])
     if not result.wasSuccessful():
-        raise Exception(f'Some tests failed. Please see log above.')
+        raise Exception(f"Some tests failed. Please see log above.")
 except IndexError:
     print("Please specify a test suite to run: i.e. 'dev' or 'all'")
