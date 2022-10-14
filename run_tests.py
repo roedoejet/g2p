6b560f030e8023e460c470a944b99b9335836091
Eric Joanis <Eric.Joanis@nrc-cnrc.gc.ca>
2022-10-14 12:57:03 -0400

    test: add --describe option to run.py and exit 1 on error
    the --describe option lists the test cases included in the specified
    test suite, and also lists which test cases are excluded, as compared to
    using "all" which discovers all tests in the current directory.

--- run_tests.py
+++ run_tests.py
@@ -6,7 +6,9 @@ from g2p.tests.run import run_tests
 
 try:
     result = run_tests(sys.argv[1])
-    if not result.wasSuccessful():
-        raise Exception("Some tests failed. Please see log above.")
+    if not result:
+        print("Some tests failed. Please see log above.")
+        sys.exit(1)
 except IndexError:
     print("Please specify a test suite to run: i.e. 'dev' or 'all'")
+    sys.exit(1)
