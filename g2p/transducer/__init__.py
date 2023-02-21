ce0b2a7361ff3e5485fd0ce621a4a94b4ae5ba8d
littell <Patrick.Littell@nrc-cnrc.gc.ca>
2023-02-21 15:06:13 -0500

    Fix for Arabic text
    There were two errors caused by Arabic text, leading to 422 and 500 errors in Studio Web when certain Arabic words were pasted in.
    
    (1) Letters in presentation forms (e.g. U+FEB7 ARABIC LETTER SHEEN INITIAL FORM) isn't understood by text-unidecode.  Getting rid of the positional information requires a K normalization, like NFKC.
    
    (2) If text-unidecode turns a character to a non-alpha character, this causes trouble downstream.  Many Arabic letters result in non-alpha characters.  We used to strip these out, but this got lost in the port from Studio to G2P.  I've added it back in.

--- g2p/transducer/__init__.py
+++ g2p/transducer/__init__.py
@@ -8,6 +8,7 @@ import copy
 import re
 from collections import defaultdict
 from typing import Dict, List
+import unicodedata
 
 import text_unidecode
 
@@ -522,7 +523,9 @@ class Transducer:
         tg = TransductionGraph(to_convert)
 
         # Conversion is done character by character using unidecode
-        converted = [text_unidecode.unidecode(c) for c in to_convert]
+        converted = [unicodedata.normalize("NFKC", c) for c in to_convert]
+        converted = [text_unidecode.unidecode(c) for c in converted]
+        converted = [c if c.isalpha() else "" for c in converted]
         tg.output_string = "".join(converted)
 
         # Edges are calculated to follow the conversion step by step
@@ -671,7 +674,6 @@ class Transducer:
         tg.edges.sort(key=lambda x: x[0])
         for i, edge in enumerate(tg.edges):
             if edge[1] is None:
-
                 # if previous exists, use that, otherwise use following, otherwise None
                 previous = [x for x in tg.edges[:i] if x[1] is not None]
                 try:
