6dad81abb43dd3259c3769e98faa32d2b7faee95
David Huggins-Daines <dhd@ecolingui.ca>
2023-03-06 14:11:51 -0500

    fix: remove spaces in `sanitize_unidecode_output` as suggested by @littell

--- g2p/tests/test_unidecode_transducer.py
+++ g2p/tests/test_unidecode_transducer.py
@@ -15,7 +15,7 @@ class UnidecodeTransducerTest(TestCase):
         self.assertEqual(m.kwargs["type"], "unidecode")
         t = Transducer(m)
         tg = t("été Nunavut ᓄᓇᕗᑦ")
-        self.assertEqual(tg.output_string, "ete Nunavut nonafot")
+        self.assertEqual(tg.output_string, "eteNunavutnonafot")
 
     def test_unidecode_g2p(self):
         transducer = make_g2p("und", "und-ascii")
@@ -38,36 +38,21 @@ class UnidecodeTransducerTest(TestCase):
         transducer = make_g2p("und", "eng-arpabet")
         tg = transducer("été Nunavut ᓄᓇᕗᑦ")
         self.assertEqual(tg.output_string, "EY T EY N UW N AA V UW T N OW N AA F OW T ")
-        # NOTE: spaces are deleted in the output, but they should be
-        # retained in the input alignments and thus recoverable
-        pe = tg.pretty_edges()
-        self.assertIn((" ", " "), (tuple(x) for x in pe[0]))  # present in und-ascii
-        self.assertNotIn((" ", " "), (tuple(x) for x in pe[1]))  # absent in und-ipa
 
     def test_unidecode_arabic_to_arpabet(self):
         transducer = make_g2p("und", "eng-arpabet")
         tg = transducer("السلام عليكم")
         self.assertEqual(tg.output_string, "L S L M L Y K M ")
-        pe = tg.pretty_edges()
-        self.assertIn((" ", " "), (tuple(x) for x in pe[0]))  # present in und-ascii
-        self.assertNotIn((" ", " "), (tuple(x) for x in pe[1]))  # absent in und-ipa
 
     def test_unidecode_arabic_presentation_to_arpabet(self):
         transducer = make_g2p("und", "eng-arpabet")
         tg = transducer("ﺷﻜﺮﺍﹰ")
         self.assertEqual(tg.output_string, "S HH K D AA N ")
-        # No input spaces, so no spaces in first transduction
-        pe = tg.pretty_edges()
-        self.assertNotIn((" ", " "), (tuple(x) for x in pe[0]))
-        self.assertNotIn((" ", " "), (tuple(x) for x in pe[1]))
 
     def test_unidecode_kanji_to_arpabet(self):
         transducer = make_g2p("und", "eng-arpabet")
         tg = transducer("日本語")
         self.assertEqual(tg.output_string, "D IY B EY N Y UW ")
-        pe = tg.pretty_edges()
-        self.assertNotIn((" ", " "), (tuple(x) for x in pe[0]))
-        self.assertNotIn((" ", " "), (tuple(x) for x in pe[1]))
 
     def test_unidecode_hanzi_to_arpabet(self):
         transducer = make_g2p("und", "eng-arpabet")
@@ -76,9 +61,6 @@ class UnidecodeTransducerTest(TestCase):
             tg.output_string,
             "N IY M EY N HH AA OW N IY M EY N S HH UW OW Y IY Y UW M AA HH ",
         )
-        pe = tg.pretty_edges()
-        self.assertNotIn((" ", " "), (tuple(x) for x in pe[0]))
-        self.assertNotIn((" ", " "), (tuple(x) for x in pe[1]))
 
 
 if __name__ == "__main__":
