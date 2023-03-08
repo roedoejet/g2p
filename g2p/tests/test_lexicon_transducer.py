22de19b209666ac10396cb6e412cd90e558a91c5
David Huggins-Daines <dhd@ecolingui.ca>
2023-03-08 11:27:44 -0500

    test: add test of lexicon loading from config file

--- g2p/tests/test_lexicon_transducer.py
+++ g2p/tests/test_lexicon_transducer.py
@@ -4,6 +4,7 @@ import os
 from unittest import TestCase, main
 
 from g2p import make_g2p
+from g2p.exceptions import MalformedMapping
 from g2p.mappings import Mapping
 from g2p.tests.public import __file__ as public_data
 from g2p.transducer import Transducer
@@ -11,6 +12,7 @@ from g2p.transducer import Transducer
 
 class LexiconTransducerTest(TestCase):
     def test_lexicon_mapping(self):
+        """Test loading a lexicon mapping directly in the constructor."""
         m = Mapping(
             type="lexicon",
             case_sensitive=False,
@@ -34,7 +36,33 @@ class LexiconTransducerTest(TestCase):
             [(0, 0), (1, 2), (1, 3), (2, 2), (2, 3), (3, None), (4, 5), (5, 5)],
         )
 
+    def test_load_lexicon_mapping(self):
+        """Test loading a lexicon mapping through a config file."""
+        m = Mapping(
+            os.path.join(
+                os.path.dirname(public_data), "mappings", "lexicon_config.yaml"
+            )
+        )
+        self.assertEqual(m.mapping, [])
+        self.assertEqual(m.kwargs["type"], "lexicon")
+        t = Transducer(m)
+        tg = t("hello")
+        self.assertEqual(tg.output_string, "HH EH L OW ")
+        self.assertEqual(
+            tg.edges, [(0, 0), (0, 1), (1, 3), (1, 4), (2, 6), (3, 6), (4, 8), (4, 9)]
+        )
+
+    def test_bad_lexicon_mapping(self):
+        """Test failure to load alignments."""
+        with self.assertRaises(MalformedMapping):
+            _ = Mapping(
+                os.path.join(
+                    os.path.dirname(public_data), "mappings", "bad_lexicon_config.yaml"
+                )
+            )
+
     def test_eng_lexicon(self):
+        """Test the cached eng to eng-ipa lexicon as a Mapping."""
         m = Mapping(in_lang="eng", out_lang="eng-ipa")
         self.assertEqual(m.kwargs["type"], "lexicon")
         t = Transducer(m)
@@ -63,7 +91,6 @@ class LexiconTransducerTest(TestCase):
                 (5, None),
             ],
         )
-        # Test insertions and deletions
         tg = t("chain")
         # These aligments are weird but they are the ones EM gave us
         self.assertEqual(tg.output_string, "tʃeɪn")
@@ -80,6 +107,8 @@ class LexiconTransducerTest(TestCase):
         )
 
     def test_eng_transducer(self):
+        """Test the cached eng to eng-ipa lexicon from make_g2p
+        ."""
         transducer = make_g2p("eng", "eng-arpabet")
         tg = transducer("hello")
         self.assertEqual(tg.output_string, "HH AH L OW ")
