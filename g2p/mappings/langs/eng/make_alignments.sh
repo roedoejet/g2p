bb91b1246f7fadb810db2894e3490a5a5c4c3c98
David Huggins-Daines <dhd@ecolingui.ca>
2023-03-27 14:05:52 -0400

    feat: switch script to use phonetisaurus from PyPI

--- g2p/mappings/langs/eng/make_alignments.sh
+++ g2p/mappings/langs/eng/make_alignments.sh
@@ -3,11 +3,12 @@
 # Replace with actual path!
 # cmudict.dict is retrievable from https://github.com/cmusphinx/cmudict/blob/master/cmudict.dict
 CMUDICT=../../../../../cmudict/cmudict.dict
-# Instructions for installing Phonetisaurus are here: https://github.com/AdolfVonKleist/Phonetisaurus
-ALIGN=../../../../../Phonetisaurus/phonetisaurus-align
+# Install Phonetisaurus with `pip install phonetisaurus`
+export PATH=$(python -c 'import phonetisaurus as p; print(p.guess_environment()["PATH"])')
+export LD_LIBRARY_PATH=$(python -c 'import phonetisaurus as p; print(p.guess_environment()["LD_LIBRARY_PATH"])')
 
 python make_ipa_cmudict.py < $CMUDICT > tmp.txt
-$ALIGN --s1_char_delim="" --s2_char_delim="" \
+phonetisaurus-align --s1_char_delim="" --s2_char_delim="" \
     --seq1_del=true --seq2_del=true --seq1_max=2 --seq2_max=2 \
     --iter=5 --input=tmp.txt --ofile=cmudict.ipa.aligned.txt
 rm tmp.txt
