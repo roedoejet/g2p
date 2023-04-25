2be51f89a38506ed9ecd5da172232961ac550c6b
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2023-04-25 17:05:42 -0400

    fix: g2p needs to update both generated .pkl and .json files
    Fixes: #237

--- g2p/mappings/langs/__init__.py
+++ g2p/mappings/langs/__init__.py
@@ -24,9 +24,6 @@ def load_langs(path: str = LANGS_PKL):
         return {}
 
 
-LANGS = load_langs()
-
-
 def load_network(path: str = LANGS_NWORK_PATH):
     try:
         return read_gpickle(path)
@@ -35,7 +32,7 @@ def load_network(path: str = LANGS_NWORK_PATH):
         return DiGraph()
 
 
-def get_available_languages(langs: dict = LANGS) -> list:
+def get_available_languages(langs: dict) -> list:
     language_names = set()
     for k, v in langs.items():
         if k in ["generated", "font-encodings"]:
@@ -49,9 +46,9 @@ def get_available_languages(langs: dict = LANGS) -> list:
     return sorted(language_names)
 
 
-def get_available_mappings(langs: dict = LANGS) -> list:
+def get_available_mappings(langs: dict) -> list:
     mappings_available = []
-    for k, v in LANGS.items():
+    for k, v in langs.items():
         if "mappings" in v:
             mappings_available.extend(v["mappings"])
         else:
@@ -59,6 +56,31 @@ def get_available_mappings(langs: dict = LANGS) -> list:
     return mappings_available
 
 
+LANGS = load_langs()
 LANGS_NETWORK = load_network()
-LANGS_AVAILABLE = get_available_languages()
-MAPPINGS_AVAILABLE = get_available_mappings()
+LANGS_AVAILABLE = get_available_languages(LANGS)
+MAPPINGS_AVAILABLE = get_available_mappings(LANGS)
+
+
+def reload_db():
+    """Reload the langs and network data, necessary after a g2p update."""
+
+    # We update all structures in place, so that another module having done from
+    # g2p.mappings.langs import VAR will see the udpates without any code changes.
+
+    global LANGS
+    LANGS.clear()
+    LANGS.update(load_langs())
+
+    global LANGS_NETWORK
+    LANGS_NETWORK.clear()
+    new_langs_network = load_network()
+    LANGS_NETWORK.update(new_langs_network.edges, new_langs_network.nodes)
+
+    global LANGS_AVAILABLE
+    LANGS_AVAILABLE.clear()
+    LANGS_AVAILABLE.extend(get_available_languages(LANGS))
+
+    global MAPPINGS_AVAILABLE
+    MAPPINGS_AVAILABLE.clear()
+    MAPPINGS_AVAILABLE.extend(get_available_mappings(LANGS))
