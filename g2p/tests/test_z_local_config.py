db1232645f2a4f1e4cf9075d282a49f354d83e0f
David Huggins-Daines <dhd@ecolingui.ca>
2024-03-26 16:13:29 -0400

    fix(test): no longer require flask needlessly for some tests

--- g2p/tests/test_z_local_config.py
+++ g2p/tests/test_z_local_config.py
@@ -21,9 +21,9 @@ from pathlib import Path
 from unittest import TestCase, main
 
 import yaml
+from click.testing import CliRunner
 
 from g2p import exceptions
-from g2p.app import APP
 from g2p.cli import convert, generate_mapping
 from g2p.mappings import Mapping
 from g2p.mappings.utils import normalize
@@ -32,7 +32,7 @@ from g2p.tests.public import PUBLIC_DIR
 
 class LocalConfigTest(TestCase):
     def setUp(self):
-        self.runner = APP.test_cli_runner()
+        self.runner = CliRunner()
         self.mappings_dir = Path(PUBLIC_DIR) / "mappings"
 
     def test_local_config(self):
