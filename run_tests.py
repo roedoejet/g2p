1262cbb872e33e6c34f464068406140383b47f46
Eric Joanis <eric.joanis@nrc-cnrc.gc.ca>
2024-11-07 15:41:08 -0500

    feat: add --quiet option to tests/run.py and refactor the runners

--- run_tests.py
+++ run_tests.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 
-import sys
+from g2p.tests.run import main
 
-from g2p.tests.run import run_tests
-
-result = run_tests("" if len(sys.argv) <= 1 else sys.argv[1])
-if not result:
-    sys.exit(1)
+main()
